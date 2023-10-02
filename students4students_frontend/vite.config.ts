27e421b35bee20788322a8d5f2e119829216b851
csknkdhbw <inf22111@lehre.dhbw-stuttgart.de>
2023-10-02 12:01:31 +0200

    usePolling to true for live debugging

--- students4students_frontend/vite.config.ts
+++ students4students_frontend/vite.config.ts
@@ -6,6 +6,11 @@ export default defineConfig({
   plugins: [react()],
 
   server: {
-    host: '0.0.0.0'
-  }
+    host: '0.0.0.0',
+    // Thanks @sergiomoura for the window fix
+    // add the next lines if you're using windows and hot reload doesn't work
+    watch: {
+      usePolling: true,
+    },
+  },
 })
